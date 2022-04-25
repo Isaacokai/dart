1aba3f505ce2ce95dbd8bcec35163281e2964404
Isaacokai <89702305+Isaacokai@users.noreply.github.com>
2022-04-25 18:07:12 +0000

    Add files via upload

---
+++ dart 1.dart
@@ -0,0 +1,13 @@
+void main(List<String> args) {
+  var number = 5;
+  var country = "Ghana";
+  var name = "Isaac";
+  var sentance = "My name is ";
+  bool ff = true;
+  bool tt = false;
+  const double pi = 3.14;
+  var answer = 4 % 3;
+  var hey = pi >= 3.14;
+  print(
+      "MY name is $name and $sentance $name I am also from $country $answer $hey");
+}
